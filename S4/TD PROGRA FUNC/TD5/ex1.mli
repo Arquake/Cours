766f4fd77bba99d08622a86537ea9df2c17908a9
Vera Nicolas <90095696+Arquake@users.noreply.github.com>
2024-02-19 07:57:30 +0100

    push

---
+++ S4/TD PROGRA FUNC/TD5/ex1.mli
@@ -0,0 +1,8 @@
+(* Q1 *)
+
+(* type quantite = int *)
+(* var quantite_topinambour : int = 12*)
+(* var quantite_patates : quantite = 9 *)
+(* type tuberrcule = Patate | Topinambour *)
+(* type stock =  *)
+
