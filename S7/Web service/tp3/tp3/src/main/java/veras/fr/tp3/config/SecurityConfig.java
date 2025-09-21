a2fcdc033aa05e71b170b633ba0dee6f6cde8f57
Arquake <arquake2@gmail.com>
2025-09-21 21:36:11 +0200

    push

---
+++ S7/Web service/tp3/tp3/src/main/java/veras/fr/tp3/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package veras.fr.tp3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
