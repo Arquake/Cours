6fe9ad774aa09fbbabcc292c0440c2bf7d0dd966
Arquake <arquake2@gmail.com>
2025-09-24 07:31:45 +0200

    Update EmployeTest.java

--- S7/Tests/TD3/tp3/src/test/java/org/example/EmployeTest.java
+++ S7/Tests/TD3/tp3/src/test/java/org/example/EmployeTest.java
@@ -1,21 +1,31 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
 
 class EmployeTest {
 
-    @Spy
     public Employe emp1;
+
+    @Spy
+    public Categorie categorie;
 
     @Test
     public void employe0to10() {
-        emp1.setSalaireDeBase(2000d);
-        emp1.setAnnee(0);
-        doReturn(2000)
-        .when(emp1).salaire();
+        emp1 = new Employe(0,2000d);
+
+        categorie = spy(new Categorie());
+
+        doReturn(1).when(categorie).valCategorie(0);
+
+        assertEquals(1, categorie.valCategorie(emp1.getAnnee()));
     }
 }
