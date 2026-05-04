c7c013fcc8dbbbc38d81a0ffa922fc640e9119a3
Vera Nicolas <arquake2@gmail.com>
2026-05-04 09:50:52 +0200

    p

--- S8/FW/bibliotheque/src/app/components/liste-livres/liste-livres.ts
+++ S8/FW/bibliotheque/src/app/components/liste-livres/liste-livres.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Livre } from '../../models/livre.model';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { LivreService } from '../../services/livre';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -13,20 +13,32 @@ import { CommonModule } from '@angular/common';
 })
 export class ListeLivres implements OnInit {
   livres: Livre[] = [];
+  livresToShow: Livre[] = []
+  currentPage: number = 1;
   isLoading: boolean = true;
   erreur: string = '';
-  constructor(private livreService: LivreService, private cdr: ChangeDetectorRef) {}
+  constructor(private livreService: LivreService, private cdr: ChangeDetectorRef, private route: ActivatedRoute, private router: Router) {}
   ngOnInit(): void {
+    this.currentPage = Number(this.route.snapshot.paramMap.get('page') || "1");
     this.livreService.getLivres().subscribe({
       next: (data) => {
-        console.log(data);
         this.livres = data;
         this.isLoading = false;
+        if (this.livres.length / 5 <= this.currentPage) {
+          this.currentPage = Math.trunc(this.currentPage / 5 + 1);
+        }
+        this.router.navigate([], {
+          relativeTo: this.route,
+          queryParams: { page: this.currentPage },
+          queryParamsHandling: 'merge'
+        });
+        this.setLivresToShow();
         this.cdr.detectChanges();
       },
       error: (err) => {
         this.erreur = 'Impossible de charger les livres. Vérifiez que json-server est démarré.';
         this.isLoading = false;
+        this.setLivresToShow();
         this.cdr.detectChanges();
       }
     });
@@ -48,5 +60,21 @@ export class ListeLivres implements OnInit {
       l.titre.toLowerCase().includes(terme) ||
       l.auteur.toLowerCase().includes(terme)
     );
+  }
+
+  nextPage() {
+    if (this.currentPage >= this.livres.length % 5 + 1 ) return
+    this.currentPage = this.currentPage + 1;
+    this.setLivresToShow();
+  }
+
+  previousPage() {
+    if (this.currentPage === 1) return
+    this.currentPage = this.currentPage - 1;
+    this.setLivresToShow();
+  }
+
+  setLivresToShow() {
+    this.livresToShow = this.livres.filter((_,index)=>index>=(this.currentPage-1)*5 && this.currentPage*5 > index)
   }
 }
